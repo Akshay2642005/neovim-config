92e78f92c97f6e9c0f972f5d9d96d402fbb68fff
Akshay B <akshay2642005@gmail.com>
2026-08-25 19:23:22 +0530

    fix: overseer quickfix via component_aliases, remove macOS-unused build templates

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,26 +1,34 @@
 return {
   'stevearc/overseer.nvim',
   lazy = true,
-  cmd = { 'OverseerRun', 'OverseerOpen', 'OverseerToggle', 'OverseerInfo', 'OverseerBuild', 'OverseerQuickAction', 'OverseerTaskAction', 'OverseerClearCache' },
+  cmd = {
+    'OverseerRun',
+    'OverseerOpen',
+    'OverseerToggle',
+    'OverseerInfo',
+    'OverseerBuild',
+    'OverseerQuickAction',
+    'OverseerTaskAction',
+    'OverseerClearCache',
+  },
   ---@module 'overseer'
   ---@type overseer.SetupOpts
   opts = {
     dap = false,
     task_list = {
-      direction = "bottom"
+      direction = 'bottom',
     },
     output = {
       use_terminal = false,
-      preserve_output = false
+      preserve_output = false,
     },
-    task_builder = function(self, params)
-      local components = { unpack(self.components) }
-      -- Inject quickfix + error notify for all tasks
-      table.insert(components, 1, { "on_output_quickfix", open = true, focus = true })
-      table.insert(components, 2, { "on_complete_notify", statuses = { "FAILURE" } })
-      return vim.tbl_extend("force", self, {
-        components = components,
-      })
-    end,
+    component_aliases = {
+      default = {
+        -- { "on_output_quickfix", open = true, focus = true },
+        'on_exit_set_status',
+        { 'on_complete_notify', statuses = { 'FAILURE' } },
+        { 'on_complete_dispose', require_view = { 'SUCCESS', 'FAILURE' } },
+      },
+    },
   },
 }
